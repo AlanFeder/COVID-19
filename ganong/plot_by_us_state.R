173e88e655c1091df35c4d8aa27803a65d1eec5b
federaj <alan.feder@invesco.com>
2020-03-28 21:38:22 -0400

    with peter updates

--- ganong/plot_by_us_state.R
+++ ganong/plot_by_us_state.R
@@ -11,9 +11,9 @@ read_day <- function(date, state, state_var_name_, metric_var_name_) {
 }
 
 read_month <- function(state, ...) {
+  # this function extracts data from March 10 forward
+  # Why? assembling state-level data for days before requires further data cleaning
   bind_rows(
-    #note: days before March 10th do not to have state-level data
-    #without additional cleaning first
     seq(10, 21) %>% map_dfr(read_day, state, `Province/State`, ...),
     seq(22, 25) %>% map_dfr(read_day, state, Province_State, ...)
   ) %>%
@@ -26,6 +26,8 @@ df <- c("New York", "Illinois", "Washington", "Florida") %>%
 df_deaths <- c("New York", "Illinois", "Washington", "Florida") %>%
   map_dfr(read_month, metric_var_name_ = Deaths)
 
+write_csv(df, "ganong/2020_03_25_state_counts.csv")
+
 (df %>%
   ggplot(aes(x = day, y = n, group = state, color = state)) +
   geom_line() +
@@ -33,7 +35,7 @@ df_deaths <- c("New York", "Illinois", "Washington", "Florida") %>%
   scale_y_log10(breaks = c(3, 10, 30, 100, 300, 1000, 3000, 10000, 30000), limits = c(3, NA)) +
   scale_x_date(date_breaks = "3 days",
                date_labels = "%b %d")) %>%
-  ggsave("ganong/state_level_plot.png", .)
+  ggsave("ganong/2020_03_25_state_level_plot.png", .)
 
 (df_deaths %>%
     ggplot(aes(x = day, y = n, group = state, color = state)) +
